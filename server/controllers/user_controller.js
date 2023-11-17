882fc27f57041c5bd265517ea47132446eea2572
Rodolfo <rodolfoespinosa01@gmail.com>
2023-11-17 11:53:12 -0500

    User can be registered through insomnia

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,7 +1,17 @@
+const User = require("../models/User");
+
 const user_controller = {
   // register User
-  register(req, res) {
-    res.send("Yep, works");
+  async register(req, res) {
+    try {
+      const user = await User.create(req.body);
+
+      res.json(user);
+    } catch (error) {
+      res.status(403).send({
+        message: error.message,
+      });
+    }
   },
 };
 
--- server/server.js
+++ server/server.js
@@ -5,9 +5,15 @@ const PORT = process.env.PORT || 3553;
 
 const user_routes = require("./routes/user_routes");
 
+const db = require("./config/connection");
+
 app.use(express.json());
 
 // Load Routes
 app.use("/auth", user_routes);
 
-app.listen(PORT, () => console.log("Server Listening on port", PORT));
+// Validate that the mongoose conenction is complete
+db.once("open", () => {
+  console.log("DB connection established");
+  app.listen(PORT, () => console.log("Server Listening on port", PORT));
+});
